9c9d2ddd4a74d6cd3d93c826e041af4af2319a53
Diane Defores <deforesd@gmail.com>
2024-11-26 02:03:15 +0100

    pseudocode

---
+++ main.js
@@ -0,0 +1,28 @@
+controlling the UI
+   adding an icon button next to every youtube links
+   adding the embedded player
+      it should be able to play, pause, stop, seek, go to next and previous video
+
+actions
+   click on any youtube button embeds the YT player inside the note 
+   play/pause the video
+   stop the video
+   seek forward 10s
+   seek backward 10s
+   go to next video
+   go to previous video
+   normal speed
+   increase speed
+   decrease speed
+
+hotkeys
+   play/pause MAJ + SPACE
+   write timestamp ALT + T
+   go to next video CTRL + RIGHT ARROW
+   go to previous video CTRL + LEFT ARROW
+   normal speed ctrl + 1
+   increase speed ctrl + 2
+   decrease speed ctrl + 3
+   seek forward 10s MAJ + right
+   seek backward 10s MAJ + left
+   
